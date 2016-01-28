25a81e751adfdc90905fede259ab3e549dd4d846
csala <c.sala.stq@gmail.com>
2016-01-28 15:35:37 +0100

    initial version

---
+++ deflate.lua
@@ -0,0 +1,14 @@
+-- Debian packages nginx-extras, lua-zlib required
+
+local content_encoding = ngx.req.get_headers()["Content-Encoding"]
+if content_encoding ~= "gzip" then
+    ngx.req.read_body()
+    local data = ngx.req.get_body_data()
+
+    if data ~= '' then
+        local deflated = require("zlib").deflate()(data, "finish")
+
+        ngx.req.set_header("Content-Encoding", "gzip")
+        ngx.req.set_body_data(deflated)
+    end
+end
---
+++ inflate.lua
@@ -0,0 +1,15 @@
+-- Debian packages nginx-extras, lua-zlib required
+
+local content_encoding = ngx.req.get_headers()["Content-Encoding"]
+if content_encoding == "gzip" then
+    ngx.req.read_body()
+    local data = ngx.req.get_body_data()
+
+    if data ~= '' then
+        local inflated = require("zlib").inflate()(data, "finish")
+
+        ngx.req.clear_header("Content-Encoding")
+        ngx.req.clear_header("Content-Length")
+        ngx.req.set_body_data(inflated)
+    end
+end
